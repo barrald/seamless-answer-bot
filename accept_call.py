bb1e503f563ac844a7af7e5e5ae5a8b6a8d668fd
barrald <barrald1213@gmail.com>
2019-04-08 22:53:06 -0400

    clean up

--- accept_call.py
+++ accept_call.py
@@ -3,9 +3,13 @@ from twilio.twiml.voice_response import VoiceResponse
 from twilio.twiml.messaging_response import MessagingResponse
 from twilio.rest import Client
 
-import creds
-client = Client(creds.account_sid, creds.auth_token)
+to_number = os.environ.get('to_number')
+from_number = os.environ.get('from_number')
 
+account_sid = os.environ.get('account_sid')
+auth_token = os.environ.get('auth_token')
+
+client = Client(account_sid, auth_token)
 
 app = Flask(__name__)
 
@@ -14,21 +18,17 @@ def answer_call():
 	message = client.messages \
 				.create(
 					body="Your food is here",
-					from_='+15172539971',
-					to='+15172904116'
+					from_= from_number,
+					to= to_number
 				)
 
 	print(message.sid)
 
 	resp = VoiceResponse()
 
-	resp.say("Hi, please leave the food on the table on four, thanks!", voice='alice', loop=3)
+	resp.say("Hi, please leave the food on the table on four, thanks!", voice='male', loop=3)
 
 	return str(resp)
-
-@app.route("/", methods=['GET', 'POST'])
-def hello():
-    return "Hello World!"
 
 if __name__ == "__main__":
 	app.run(debug=True)
